7f31de2c3499adeafa5075c019790bcaf1f5b801
iyabc <juliennepanes@gmail.com>
2024-03-16 03:57:09 +0800

    chore(FooterHeight): fix changing height on raise click on footer | to merge from main after issue 10

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -52,139 +52,149 @@ const Footer = () => {
     };
 
     return (
-        <Flex justifyContent={'end'} gap={3} p={2}>
-            {showRaise ? (
-                <Flex gap={2} alignItems={'center'}>
-                    <Box
-                        bg={'charcoal.800'}
-                        width={'fit-content'}
-                        textAlign={'center'}
-                        rounded={'lg'}
-                        px={0.5}
-                        flex={1}
-                    >
-                        <Text whiteSpace={'nowrap'} p={1} fontSize={'sm'}>
-                            Your Bet
-                        </Text>
-                        <Input
-                            bg={'charcoal.600'}
-                            border={'white'}
-                            fontSize={'xl'}
-                            mb={1}
-                            type="number"
-                            value={inputValue}
-                            min={minRaise}
-                            max={maxRaise}
-                            onChange={handleInputChange}
-                            focusBorderColor={'gray.300'}
+        <Flex
+            justifyContent={'end'}
+            gap={3}
+            p={2}
+            height={150}
+            overflow={'hidden'}
+        >
+            <Flex gap={2} alignItems={'center'}>
+                {showRaise ? (
+                    <>
+                        <Box
+                            bg={'charcoal.800'}
+                            width={'fit-content'}
                             textAlign={'center'}
-                            onFocus={() => setIsInputFocus(true)}
-                            onBlur={handleInputOnBlur}
-                        />
-                    </Box>
-                    <Flex
-                        flexDirection={'column'}
-                        bg={'charcoal.800'}
-                        rounded={'lg'}
-                        flex={1}
-                        justifyContent={'space-between'}
-                        height={'100%'}
-                        overflow={'hdden'}
-                    >
-                        <Flex flex={1} gap={2} p={2}>
-                            <Button variant={'raiseActionButton'}>
-                                Min Raise
-                            </Button>
-                            <Button variant={'raiseActionButton'}>
-                                1/2 Pot
-                            </Button>
-                            <Button variant={'raiseActionButton'}>
-                                3/4 Pot
-                            </Button>
-                            <Button variant={'raiseActionButton'}>Pot</Button>
-                            <Button variant={'raiseActionButton'}>
-                                All In
-                            </Button>
-                        </Flex>
-                        <Flex
-                            alignItems={'center'}
+                            rounded={'lg'}
+                            px={0.5}
                             flex={1}
-                            bg={'charcoal.600'}
-                            roundedBottom={'lg'}
-                            overflow={'hidden'}
                         >
-                            <Flex
-                                bg={'charcoal.400'}
-                                height={'100%'}
-                                alignItems={'center'}
-                                p={1}
-                            >
-                                <LuMinus />
-                            </Flex>
-                            <Slider
-                                aria-label="slider-ex-1"
-                                marginX={3}
-                                defaultValue={0}
-                                value={sliderValue}
-                                max={maxRaise}
+                            <Text whiteSpace={'nowrap'} p={1} fontSize={'sm'}>
+                                Your Bet
+                            </Text>
+                            <Input
+                                bg={'charcoal.600'}
+                                border={'white'}
+                                fontSize={'xl'}
+                                mb={1}
+                                type="number"
+                                value={inputValue}
                                 min={minRaise}
-                                onChange={(value: number) =>
-                                    handleSliderChange(value)
-                                }
-                                isDisabled={isInputFocus}
-                            >
-                                <SliderTrack>
-                                    <SliderFilledTrack />
-                                </SliderTrack>
-                                <SliderThumb />
-                            </Slider>
+                                max={maxRaise}
+                                onChange={handleInputChange}
+                                focusBorderColor={'gray.300'}
+                                textAlign={'center'}
+                                onFocus={() => setIsInputFocus(true)}
+                                onBlur={handleInputOnBlur}
+                            />
+                        </Box>
+                        <Flex
+                            flexDirection={'column'}
+                            bg={'charcoal.800'}
+                            rounded={'lg'}
+                            flex={1}
+                            justifyContent={'space-between'}
+                            height={'100%'}
+                            overflow={'hdden'}
+                        >
+                            <Flex flex={1} gap={2} p={2}>
+                                <Button variant={'raiseActionButton'}>
+                                    Min Raise
+                                </Button>
+                                <Button variant={'raiseActionButton'}>
+                                    1/2 Pot
+                                </Button>
+                                <Button variant={'raiseActionButton'}>
+                                    3/4 Pot
+                                </Button>
+                                <Button variant={'raiseActionButton'}>
+                                    Pot
+                                </Button>
+                                <Button variant={'raiseActionButton'}>
+                                    All In
+                                </Button>
+                            </Flex>
                             <Flex
-                                bg={'charcoal.400'}
-                                height={'100%'}
                                 alignItems={'center'}
-                                p={1}
+                                flex={1}
+                                bg={'charcoal.600'}
+                                roundedBottom={'lg'}
+                                overflow={'hidden'}
                             >
-                                <LuPlus />
+                                <Flex
+                                    bg={'charcoal.400'}
+                                    height={'100%'}
+                                    alignItems={'center'}
+                                    p={1}
+                                >
+                                    <LuMinus />
+                                </Flex>
+                                <Slider
+                                    aria-label="slider-ex-1"
+                                    marginX={3}
+                                    defaultValue={0}
+                                    value={sliderValue}
+                                    max={maxRaise}
+                                    min={minRaise}
+                                    onChange={(value: number) =>
+                                        handleSliderChange(value)
+                                    }
+                                    isDisabled={isInputFocus}
+                                >
+                                    <SliderTrack>
+                                        <SliderFilledTrack />
+                                    </SliderTrack>
+                                    <SliderThumb />
+                                </Slider>
+                                <Flex
+                                    bg={'charcoal.400'}
+                                    height={'100%'}
+                                    alignItems={'center'}
+                                    p={1}
+                                >
+                                    <LuPlus />
+                                </Flex>
                             </Flex>
                         </Flex>
-                    </Flex>
-                    <Flex flex={1} gap={2}>
+                        <Flex flex={1} gap={2}>
+                            <ActionButton
+                                text={'Back'}
+                                color="white"
+                                clickHandler={() => setShowRaise(false)}
+                            />
+                            <ActionButton
+                                text={'Raise'}
+                                color="green"
+                                clickHandler={handleSubmitRaise}
+                            />
+                        </Flex>
+                    </>
+                ) : (
+                    <>
                         <ActionButton
-                            text={'Back'}
-                            color="white"
-                            clickHandler={() => setShowRaise(false)}
+                            text={'Call'}
+                            color="green"
+                            clickHandler={() => console.log('Call Clicked')}
                         />
                         <ActionButton
                             text={'Raise'}
                             color="green"
-                            clickHandler={handleSubmitRaise}
+                            clickHandler={handleRaiseOnClick}
                         />
-                    </Flex>
-                </Flex>
-            ) : (
-                <Flex gap={2}>
-                    <ActionButton
-                        text={'Call'}
-                        color="green"
-                        clickHandler={() => console.log('Call Clicked')}
-                    />
-                    <ActionButton
-                        text={'Raise'}
-                        color="green"
-                        clickHandler={handleRaiseOnClick}
-                    />
-                    <ActionButton
-                        text={'Check'}
-                        color="green"
-                        clickHandler={() => console.log('Check Clicked')}
-                    />
-                    <ActionButton
-                        text={'Fold'}
-                        color="red"
-                        clickHandler={() => console.log('Fold Clicked')}
-                    />
-                </Flex>
-            )}
+                        <ActionButton
+                            text={'Check'}
+                            color="green"
+                            clickHandler={() => console.log('Check Clicked')}
+                        />
+                        <ActionButton
+                            text={'Fold'}
+                            color="red"
+                            clickHandler={() => console.log('Fold Clicked')}
+                        />
+                    </>
+                )}
+            </Flex>
         </Flex>
     );
 };

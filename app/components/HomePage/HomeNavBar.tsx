4abf513ef888c6a7c2204d4deaa56d58a415880b
Denis Ivanov <denisivanov@Deniss-MacBook-Pro-2.local>
2024-10-15 16:02:31 +0300

    fix HomeNavBar to not be present on /game pages

--- app/components/HomePage/HomeNavBar.tsx
+++ app/components/HomePage/HomeNavBar.tsx
@@ -16,6 +16,7 @@ import React from 'react';
 import { RiMenu3Line } from 'react-icons/ri';
 import Web3Button from '../Web3Button';
 import { Poppins } from 'next/font/google';
+import { usePathname } from 'next/navigation'; // Import the hook
 
 const poppins = Poppins({
     weight: ['700'],
@@ -27,6 +28,12 @@ const logoImage = '/logo.png';
 
 const HomeNavBar: React.FC = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
+
+    const pathname = usePathname();
+    const showHomeNavBar = !pathname.startsWith('/game');
+    if (!showHomeNavBar) {
+        return null; // Do not render the navbar on /game pages
+    }
 
     const NavButtons = React.memo(() => (
         <>

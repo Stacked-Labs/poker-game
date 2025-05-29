71c1b0467b3c7426e062786b602fefa877c3f1ba
juliennepanes <57930589+iyabc@users.noreply.github.com>
2025-05-29 18:44:52 +0800

    code clean up #139

--- app/components/Table.tsx
+++ app/components/Table.tsx
@@ -28,16 +28,16 @@ const initialPlayers: (Player | null)[] = [
 ];
 
 const seatIndices = [
-    'one',
-    'two',
-    'three',
-    'four',
-    'five',
-    'six',
-    'seven',
-    'eight',
-    'nine',
-    'ten',
+    { id: 'one', value: 1 },
+    { id: 'two', value: 2 },
+    { id: 'three', value: 3 },
+    { id: 'four', value: 4 },
+    { id: 'five', value: 5 },
+    { id: 'six', value: 6 },
+    { id: 'seven', value: 7 },
+    { id: 'eight', value: 8 },
+    { id: 'nine', value: 9 },
+    { id: 'ten', value: 10 },
 ];
 
 const templateGridLarge = `"a five six seven b"

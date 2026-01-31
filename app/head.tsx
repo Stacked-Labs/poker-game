29ecb31def320a936965cac05e49b7727292f392
Denis Ivanov <denisivanov@Deniss-MacBook-Pro-2.local>
2026-01-31 18:33:09 +0200

    add baseid to head fallback aswell

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+        <>
+            <meta
+                name="base:app_id"
+                content="697e0df32aaf0bc9ad8a2b7"
+            />
+        </>
+    );
+}

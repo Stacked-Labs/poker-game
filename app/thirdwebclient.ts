21379d0095d01aea1a83089d3733a1204cbf0b21
Denis Ivanov <denisivanov@Mac.attlocal.net>
2026-03-17 12:03:01 -0500

    build fix

--- app/thirdwebclient.ts
+++ app/thirdwebclient.ts
@@ -2,14 +2,10 @@ import { createThirdwebClient } from 'thirdweb';
 import { base, baseSepolia } from 'thirdweb/chains';
 import { createWallet, inAppWallet } from 'thirdweb/wallets';
 
-const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
-
-if (!clientId) {
-    throw new Error('Client ID not found');
-}
+const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID ?? '';
 
 export const client = createThirdwebClient({
-    clientId: clientId,
+    clientId,
 });
 
 // Chain configuration - Base mainnet
